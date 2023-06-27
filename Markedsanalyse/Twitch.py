d08710f95fe2e867cc25e125b544acd582e2ba0c
Thomas Kjølbye <thomaskjolbye@hotmail.com>
2023-06-27 12:59:49 +0200

    Twitch get access token

--- Markedsanalyse/Twitch.py
+++ Markedsanalyse/Twitch.py
@@ -1,5 +1,38 @@
 import os
 import requests
 
+# Load client ID and secret as environmental variables
+client_id = os.environ.get("TWITCH_CLIENT_ID")
+client_secret = os.environ.get("TWITCH_CLIENT_SECRET")
 
-print("Hello World!")
+
+# Base URL for all top games
+top_games_url = "https://api.twitch.tv/helix/games/top"
+
+
+def get_access_token():
+    "get_access_token returnerer et access_token, som vi skal bruge til Twitch API"
+
+    access_token = os.environ.get("TWITCH_ACCESS_TOKEN")
+    if access_token:
+        return access_token
+
+    # Generate access token
+    auth_body = {"client_id": client_id,
+                 "client_secret": client_secret,
+                 "grant_type": "client_credentials"
+                 }
+
+    auth_response = requests.post(
+        url="https://id.twitch.tv/oauth2/token", json=auth_body)
+
+    # Set up headers for future requests
+    auth_response_json = auth_response.json()
+
+    # Set Environment Variables and return token
+    os.environ["TWITCH_ACCESS_TOKEN"] = auth_response_json['access_token']
+    return os.environ.get("TWTICH_ACCESS_TOKEN")
+
+
+def validate_auth(access_token):
+    "validate_auth tjekker om access token er valid, ellers får vi en ny i get_access_token"
