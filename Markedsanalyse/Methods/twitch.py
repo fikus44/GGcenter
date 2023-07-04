ac0ed3c7d19e58237e3039a969234ce7ca9fd510
Thomas Kjølbye <thomaskjolbye@hotmail.com>
2023-07-04 20:53:25 +0200

    Comments to the code

--- Markedsanalyse/Methods/twitch.py
+++ Markedsanalyse/Methods/twitch.py
@@ -1,19 +1,30 @@
 import os
 import requests
-import time
+
+'''
+Twitch.py encloses all methods to extract data from
+the Twitch API pertaining to viewers per game and
+viewers per game per country.
+
+'''
 
 # Load client ID and secret as environmental variables
 client_id = os.environ.get("TWITCH_CLIENT_ID")
 client_secret = os.environ.get("TWITCH_CLIENT_SECRET")
 
 
-# Base URL for all top games
+# Base URL for all requests
 top_games_url = "https://api.twitch.tv/helix/games/top"
 top_streams_url = "https://api.twitch.tv/helix/streams"
 
 
 def get_access_token():
-    # get_access_token returnerer et valid access_token, som vi skal bruge til Twitch API
+
+    '''
+    get_access_token() returns a valid access token used
+    to make HTTP requests to the Twitch API. 
+
+    '''
 
     access_token = os.environ.get("TWITCH_ACCESS_TOKEN")
     valid_access_token = validate_access_token(access_token)
@@ -22,7 +33,11 @@ def get_access_token():
     return generate_access_token()
 
 def generate_access_token():
-    # generate_access_token genererer et access token, hvis det nuværende er invalid
+
+    '''
+    generate_access_token() generates a valid access token
+    
+    '''
 
     # Generate body for HTTP post 
     auth_body = {"client_id": client_id,
@@ -41,7 +56,12 @@ def generate_access_token():
     return os.environ.get("TWTICH_ACCESS_TOKEN")
 
 def get_headers():
-    # get_headers returnerer de headers vi skal bruge i get requesten for at få data ud fra twitch API
+
+    '''
+    get_headers() returns the access token and client ID
+    used in the get HTTP requests for Twitch's API.
+    
+    '''
 
     headers = {
         "Authorization" : f"Bearer {get_access_token()}",
@@ -52,8 +72,19 @@ def get_headers():
 
 
 def validate_access_token(access_token):
-    "validate_access_token tjekker om access token er valid, ellers får vi en ny i get_access_token"
 
+    '''
+    validate_access_token() checks if the current acess token is valid. 
+    If not generate_access_token() generates a new one. 
+
+    Parameters
+    ----------
+    access_token : Str 
+        Access token to validate
+    
+    '''
+
+    # Validation URL and header
     validate_url = 'https://id.twitch.tv/oauth2/validate'
     validate_header = {"Authorization": f"OAuth {access_token}"
                     }
@@ -68,9 +99,25 @@ def validate_access_token(access_token):
 
 def top_games(params = {'first' : 50}):
 
+    '''
+    top_games() returns two lists of the (default) 50 most popular 
+    games as measured by number of viewers. The first list returns
+    the name of the game wilst the second returns the game ID. 
+
+    Parameters
+    ----------
+    params : Dictionary
+        Specified paramters for the get HTTP request.
+        Only the first parameter (# of hits per page)
+        is required. 
+    
+    '''
+
+    # Get request and parse from json to dictionary
     games_response = requests.get(url = top_games_url, params = params, headers = get_headers())
     games_response_json = games_response.json()
 
+    # Lists of name of games and IDs
     game_name = [game["name"] for game in games_response_json["data"]]
     game_id = [game["id"] for game in games_response_json["data"]]
 
@@ -79,13 +126,58 @@ def top_games(params = {'first' : 50}):
 
 def get_streams(params):
 
+    '''
+    get_streams() returns a dictionary of one page of
+    response bodies of various streams. The number
+    of streams, the chosen game, and the page number
+    is determined by the parameters.
+
+    Parameters
+    ----------
+    params : Dictionary
+        Specified paramters for the get HTTP request. 
+        The parameters are elaborated on below. 
+
+        game_id : Str / Int
+            Filters the get request to only the specified
+            games. In my setup, I go one game at a time.
+
+        first : Str
+            Sets the amounts of hits per page. 100 is maximum
+
+        after : Str
+            Returns the next page of hits. This is left blank
+            in the first run (first page)
+    
+    '''
+
+    # Get HTTP request to get streams and parse from json to dictionary
     streams_response = requests.get(url = top_streams_url, params = params, headers = get_headers())
     streams_response_json = streams_response.json()
 
     return streams_response_json
 
+
 def update_params(stream_response, game_id):
 
+    '''
+    update_params() updates the params variable used
+    in the get_streams() method such that when called
+    get_streams() returns the next page of streams
+
+    Parameters
+    ----------
+    stream_response : Dictionary
+        Output from get_streams() method. Using f-
+        formatting the parameters are then updated
+
+    game_id : Str / Int
+        Filters the parameters to only pertain to
+        the specified game
+
+    '''
+
+    # Params variable for subsequent stream requests
     params = {'game_id' : f'{game_id}', 'first' : 100, 'after' : f'{stream_response["pagination"]["cursor"]}'}
 
     return params
@@ -93,10 +185,44 @@ def update_params(stream_response, game_id):
 
 def loop_through_pages(params, game_id):
 
+    '''
+    loop_through_pages() loops through all
+    pages of the HTTP get request and returns
+    all response bodies in a list
+
+    Parameters
+    ----------
+    params : Dictionary
+        Specified paramters for the get HTTP request. 
+        The parameters are elaborated on below. 
+
+        game_id : Str / Int
+            Filters the get request to only the specified
+            games. In my setup, I go one game at a time.
+
+        first : Str
+            Sets the amounts of hits per page. 100 is maximum
+
+        after : Str
+            Returns the next page of hits. DO NOT SPECIFY IT!
+            The reason being that we specify the first run in
+            the params variable. The next pages are generated
+            using the update_params() method which generates
+            the params variable with the after parameter 
+            itself. 
+        
+    game_id : Str / Int
+        Filters the parameters to only pertain to
+        the specified game
+    
+    '''
+
+    # Initialize holder as well as first page of stream response bodies
     result = []
     stream_response = get_streams(params)
     result.extend(stream_response["data"])
 
+    # While there are new pages we keep requesting them and adding them to the holder
     while bool(stream_response["pagination"]): # Dict evaluates to True as long as there is another page 
         new_params = update_params(stream_response, game_id)
         stream_response = get_streams(new_params)
@@ -107,11 +233,47 @@ def loop_through_pages(params, game_id):
 
 def viewers_per_game(params, game_id):
 
+    '''
+    viewers_per_game() returns a list of live viewers
+    for the specified game as well as a dictionary of
+    the number of viewers from each of the 10 most
+    popular countries
+
+    Parameters
+    ----------
+    params : Dictionary
+        Specified paramters for the get HTTP request. 
+        The parameters are elaborated on below. 
+
+        game_id : Str / Int
+            Filters the get request to only the specified
+            games. In my setup, I go one game at a time.
+
+        first : Str
+            Sets the amounts of hits per page. 100 is maximum
+
+        after : Str
+            Returns the next page of hits. DO NOT SPECIFY IT!
+            The reason being that we specify the first run in
+            the params variable. The next pages are generated
+            using the update_params() method which generates
+            the params variable with the after parameter 
+            itself. 
+    
+    game_id : Str / Int
+        Filters the parameters to only pertain to
+        the specified game
+
+    '''
+
+    # Loop through pages pertaining to the specifc game and sum viewers
     stream_response = loop_through_pages(params, game_id)
     viewers_per_game = [game["viewer_count"] for game in stream_response]
 
+    # 10 most popular languages on twitch
     top10_lang = ["en", "es", "ko", "fr", "zh", "ru", "de", "it", "pt", "ja"]
 
+    # Viewers pertaining to each of the 10 most popular countries
     en_viewers_per_game = sum([game["viewer_count"] if game["language"] == 'en' else 0 for game in stream_response])
     sp_viewers_per_game = sum([game["viewer_count"] if game["language"] == 'es' else 0 for game in stream_response])
     ko_viewers_per_game = sum([game["viewer_count"] if game["language"] == 'ko' else 0 for game in stream_response])
@@ -130,12 +292,7 @@ def viewers_per_game(params, game_id):
 
     return sum(viewers_per_game), country_viewers_per_game
 
-def country_viewers_per_game(params, game_id):
-
-    stream_response = loop_through_pages(params, game_id)
-    country_viewers_per_game = []
-
-    return None 
+ 
 
 
 
