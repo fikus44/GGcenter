864ffbb53aa0be77a8a412701d258a9457a18db8
Thomas Kjølbye <thomaskjolbye@hotmail.com>
2023-07-07 22:45:37 +0200

    bat script

--- Markedsanalyse/main.py
+++ Markedsanalyse/main.py
@@ -1,12 +1,15 @@
 from methods import twitch as tw
 from methods import dataprocessing as dp
 
+print("Getting fresh data")
 
 # Request data from Twitch's API
 time, twitch_viewers, twitch_viewers_by_country = tw.loop_through_games()
 
 # Dataprocessing
 tester, tester_lang = dp.dataprocessing(dict = twitch_viewers, dict_language = twitch_viewers_by_country, time = time)
+
+print("done getting fresh data")
 
 
 # Hvad er steps herfra? Nu skal jeg automatisere scriptet således det bare kan køre uden mig og så skal jeg begynde at arbejde med data 
---
+++ Markedsanalyse/run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\thoma\Desktop\Ø-Umlaut startup\GameOnSite\GameOnSite Git\Markedsanalyse
+start python main.py
