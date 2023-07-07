79ce7c1b5ee7b869e87c2677f6d01fb8eb5dd2e5
Thomas Kjølbye <thomaskjolbye@hotmail.com>
2023-07-07 23:04:27 +0200

    output py file

--- Markedsanalyse/Methods/dataprocessing.py
+++ Markedsanalyse/Methods/dataprocessing.py
@@ -57,26 +57,3 @@ def dataprocessing(dict, dict_language, time):
     save_to_csv(merged_data, merged_data_language)
 
     return merged_data, merged_data_language
-
-
-# Når det er oppe at køre så kal dataprocessing ikke returnere noget, den slutter bare med at gemme conslidated data, ellers så skal den og så behvøer jeg ikke loade data
-# for så at arbejde med det for at lave figurer og tabeller osv. 
-
-    
-
-
-
-
-# Gem til csv eller excel (hvad er bedst) med et test sæt (koden til det) -- load det bagefter (koden til det) og så append med ny tester som så gemmes (kode til det)
-
-# Funktion som gemmer ved at appende til eksisterende data -- i starten bliver det på drive, men det skal op i skyen på et tidspunkt. Det bliver raw data jeg gemmer
-# herefter skal jeg så have et nyt ark med transformeret data f.eks. daily average. 
-
-# Der er lidt en udfordring ved at gøre det idet at de spil jeg får ud 1) ikke vil være de samme hver gang og 2) rækkefølgen på dem vil være anderledes og 3) der kan være nye spil.
-# Jeg tror måske den bedste måde, selvom den måske er lidt dum, er at loade raw data ind hver gang (det bliver bare større og større) og så merge med det datasæt. Tror ikke den bliver
-# stor nok til det bliver et issue dog. Jeg får 200 entries om dagen, på et år er det 200 * 365 = 73000, hvilket jeg har regnet mig frem til er ca 87 MB (før downcasting)
-
-# Det ville være federe at gøre det uden at loade data, men jeg skal have det loaded alligevel når jeg skal transfomere (til f.eks. averages) og så arbejde med det; måske kan linket
-# her bruges: https://stackoverflow.com/questions/76345249/pandas-to-csv-method-in-append-mode-with-dataframes-with-different-columns
-
-
---
+++ Markedsanalyse/Methods/output.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+
+# Script til at producere output
+# 1) tabel med spil i første søjle og så avg. daily viewers, weekly, 2 weeks, 1 month, 3 month 6 month and 1 year. Sorteret i descending order efter en af dem. 
+# 2) Tabel med 5 største procentuelle gain og loss (10 i alt) ift. sidste uge - så vi sammenligner altså weekly avg viewer tal med det samme tal for en uge siden
+# 3) Figur der viser 10 største spils avg daily viewers -- måske to mere på lidt længere baner; en helt kort, en mellem og en lang
+# 4) Tabel der lister nye spil, der er kommet på top 50 som ikke er set før med deres viewers sidste dag (de har ikke været på listen længere så kan ikke vise andet)
+# Nu hvor vi bruger sproget:
+# 5) Tabel med spil ad y-akse og sprog ad x-akse hvor de er sorteret i descending order efter engelske viewers. En tabel med daily, en med weekly, 2 weeks, 1 month 3 months etc. 
